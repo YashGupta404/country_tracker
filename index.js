a30646f2972662d591a93c12e28a9b75b8315027
YashGupta404 <yashgupta.work.2005@gmail.com>
2024-09-14 01:24:44 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import { titleCase } from "title-case";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "root",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(countries);
+  return countries;
+}// Function to check visited countries
+
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = titleCase(req.body["country"]);
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countires WHERE LOWER(country_name) LIKE '%' || $1 || '%';",// By mistake wrote "countires" as the table name
+      [input.toLowerCase()]
+    );// Here % means any letters or words and || is used for concatenation, LOWER is used to convert into lowercase
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }// handling errors
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
